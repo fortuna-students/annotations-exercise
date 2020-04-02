d63627fbb0b3187bb767eb9c1f0808aa7f94a73b
viviancan <vcnales@gmail.com>
2020-04-02 11:37:41 -0500

    added easy-fix

--- src/Author.java
+++ src/Author.java
@@ -3,7 +3,7 @@
  */
 import java.util.ArrayList;
 import java.util.List;
-
+@SuppressWarnings("unchecked")
 public class Author extends Person {
     private List books;
 
@@ -13,7 +13,7 @@ public class Author extends Person {
     }
 
     /**
-     * @deprecated Use publishedBooks instead
+     * @deprecated Use publishedBooks instead {@link #publishedBooks()}
      */
     @Deprecated
     public List<String> getBooks() {
@@ -28,7 +28,7 @@ public class Author extends Person {
         books.add(book);
     }
 
-    @Override
+//    @Override
     public String sortName() {
         return String.format("%s, %s", lastName, firstName);
     }
